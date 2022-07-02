525df11dfda1bf4bad3d950f42e1b659c79604b4
Sami Reddioui <63423744+samireddioui@users.noreply.github.com>
2022-07-02 17:30:40 +0100

    Add files via upload

--- armIK.m
+++ armIK.m
@@ -1,9 +1,14 @@
-function [th] = armIK(px,py,pz)
+function [th] = armIK
 
 %Link length
 L1 = input('value of link1 ...');
 L2 = input('value of link2 ...');
 L3 = input('value of link3 ...');
+%Position input of the end effector
+px = input('value of px ...');
+py = input('value of py ...');
+pz = input('value of pz ...');
+
 
 %calc theta1
 th1 = atan2(py, px);
@@ -23,4 +28,3 @@ th3 = rad2deg(th3);
 th2 = rad2deg(th2);
 th = [th1, th2, th3];
 
-end
